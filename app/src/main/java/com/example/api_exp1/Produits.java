494dc446938c2c6e22debaadb81d101c7926f6f2
unknown <tasnime.jamazi@sesame.com.tn>
2022-04-07 11:07:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/api_exp1/Produits.java
@@ -0,0 +1,13 @@
+package com.example.api_exp1 ;
+
+public class Produits {
+
+    private String nomProduit;
+
+    public Produits (String nomProduit) {
+        this.nomProduit = nomProduit;
+    }
+    public String getNomProduit() {
+        return nomProduit;
+    }
+}
